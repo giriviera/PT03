c1ed6d77404d603aeca8e083cefea12cf0950c67
Giankarlo Rivera <signmein3456@gmail.com>
2025-09-01 09:23:48 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelector('#search').addEventListener('click', getUser);
+
+function getUser(e){
+    const name = document.querySelector('#code').value;
+
+    fetch(`https://fordemo-ot4j.onrender.com/users/${name}`)
+    .then((response) => response.json())
+    .then((data) => {
+        document.querySelector('.result').innerHTML = `
+        <div>User ID: ${data.id}</div>`;
+    })
+    .catch((err) => {
+        document.querySelector('.result').innerHTML = `
+        <div style="color: red;">Error: ${err.message}</div>`;
+    });
+
+    e.preventDefault();
+}
